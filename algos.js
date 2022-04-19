c4d8fba4efd4081be4dcfeb9a8d03d6913248697
samhcarrasco <samuelcarrasco@Samuels-Air.fios-router.home>
2022-04-18 22:08:21 -0400

    max profit sliding window

--- algos.js
+++ algos.js
@@ -52,15 +52,36 @@
 //optimized solution (hashmap)
 
 /********************************************************Valid Anagram********************************************************/
-var isAnagram = function(s, t) {
-  const string = s.split('')
-  const compare = t.split('')
-  if(string.length !== compare.length ){
-    return false
-  }
-  for(let i = 0; i < string.length; i++){
-    for 
-  }
+// var isAnagram = function(s, t) {
+//   const string = s.split('')
+//   const compare = t.split('')
+//   if(string.length !== compare.length ){
+//     return false
+//   }
+//   for(let i = 0; i < string.length; i++){
+//     for 
+//   }
  
+// };
+// isAnagram("chinos","music");
+
+/***********************************************Best Time to Buy and Sell Stock*****************************************************************/
+
+var maxProfit = function(array) {
+    let p1 = 0;
+    let p2 = 1;
+    let maxProf = 0;
+    while(p1 < array.length-1){
+      if(array[p2] - array[p1] > maxProf){
+        maxProf = array[p2] - array[p1]
+      }
+      if(p2 === array.length){
+        p2 = p1 + 1
+        p1++
+      }
+      p2++
+    }
+   if(maxProf < 1){return 0}
+   return maxProf
 };
-isAnagram("chinos","music");
+
