8231d0f56140aa23e7842d516838e547d305a668
samhcarrasco <samuelcarrasco@Samuels-Air.fios-router.home>
2022-04-20 08:09:13 -0400

    added comments

--- twoSum.js
+++ twoSum.js
@@ -1,35 +1,35 @@
 /********************************************************TWOSUM********************************************************/
-// //Brute Force
+//Brute Force
 
-// //expected input: array with num values, and a num
-// //expected output: An array with 2 index positions
+//expected input: array with num values, and a num
+//expected output: An array with 2 index positions
 
-// //initialize and define twosum
-// function twoSum(array,target){
-//   //create empty array that will hold I and J values
-//   const indexPosition = [];
-//   //create our first for loop to iterate through I will refer to the first value that being added
-//   for(let i = 0; i < array.length; i++){
-//     //create second loop to iterate through J will refer to the second value being added (to I)
-//     for(let j = 1; j < array.length; j++){      
-//       //when I + J = target
-//       if(array[i]+array[j] === target){
-//        //push I and J into empty array
-//        indexPosition.push(i,j)
-//        return indexPosition
-//       }
-//     }
-//   }
-//   //return empty array
-// } 
-
-
+//initialize and define twosum
+function twoSum(array,target){
+  //create empty array that will hold I and J values
+  const indexPosition = [];
+  //create our first for loop to iterate through I will refer to the first value that being added
+  for(let i = 0; i < array.length; i++){
+    //create second loop to iterate through J will refer to the second value being added (to I)
+    for(let j = 1; j < array.length; j++){      
+      //when I + J = target
+      if(array[i]+array[j] === target){
+       //push I and J into empty array
+       indexPosition.push(i,j)
+       return indexPosition
+      }
+    }
+  }
+  //return empty array
+} 
 
 
-// //tests
-// console.log(twoSum([1,2,3],4))//[0,2]
-// console.log(twoSum([1,2,3,4],6))//[1,3]
-// console.log(twoSum([]))
+
+
+//tests
+console.log(twoSum([1,2,3],4))//[0,2]
+console.log(twoSum([1,2,3,4],6))//[1,3]
+console.log(twoSum([]))
 
 
 // //optimized solution(using hashmap)
