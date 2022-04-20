aede1b87610221d108a0d99439ed1a560e5c1187
samhcarrasco <samhcarrasco@gmail.com>
2022-04-20 09:10:51 -0400

    optimized bestTimeToBuyStock

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -1,20 +1,19 @@
 
 /***********************************************Best Time to Buy and Sell Stock*****************************************************************/
-
 var maxProfit = function(array) {
   let p1 = 0;
   let p2 = 1;
   let maxProf = 0;
-  while(p1 < array.length-1){
-    if(array[p2] - array[p1] > maxProf){
-      maxProf = array[p2] - array[p1]
+  while(p2 < array.length){
+    if(array[p1]<array[p2]){
+      let profit = array[p2] - array[p1]
+      maxProf = Math.max(maxProf,profit)
     }
-    if(p2 === array.length){
-      p2 = p1 + 1
-      p1++
+    else{
+      p1 = p2
     }
-    p2++
-  }
- if(maxProf < 1){return 0}
- return maxProf
+    p2 += 1
 };
+return maxProf
+}
+console.log(maxProfit([5,8,2,9,11,115,12,1,1123,2235234]))
