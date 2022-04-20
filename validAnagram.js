d001217689aed595e38bd404c6c9a8930b309c4f
samhcarrasco <samuelcarrasco@Samuels-Air.fios-router.home>
2022-04-20 08:06:18 -0400

    valid anagram

---
+++ validAnagram.js
@@ -0,0 +1,15 @@
+
+/********************************************************Valid Anagram********************************************************/
+ var isAnagram = function(s, t) {
+   const string = s.split('')
+   const compare = t.split('')
+   if(string.length !== compare.length ){
+     return false
+   }
+   for(let i = 0; i < string.length; i++){
+     for 
+  }
+ 
+ };
+ isAnagram("chinos","music");
+
