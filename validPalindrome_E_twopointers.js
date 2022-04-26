6553ffb57b3094e3071ebe2ee0da909a21df1a3e
samhcarrasco <samhcarrasco@gmail.com>
2022-04-26 09:49:30 -0700

    valid palindrome optimized with 2 pointers

--- validPalindrome_E_twopointers.js
+++ validPalindrome_E_twopointers.js
@@ -10,3 +10,20 @@ let reversed = "";
   return(reversed === s ? true: false)
 }
 console.log(validPalindrome("racecar"))
+
+// two pointer solution
+
+function palindrome(s){
+  let p1 = 0;
+  let p2 = s.length-1
+
+  while(p1 < p2){
+    if(s[p1] !== s[p2]){
+      return false
+    }
+    p1++
+    p2--
+  }
+  return true
+}
+console.log(palindrome("moom"))
