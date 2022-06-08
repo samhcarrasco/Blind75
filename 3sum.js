35cca007e4950955f86a375ce499053900f233d7
samhcarrasco <samhcarrasco@gmail.com>
2022-06-08 17:58:09 -0400

    2nd optimized solution

--- 3sum.js
+++ 3sum.js
@@ -31,3 +31,33 @@ var threeSum = function(nums) {
   return res
 };
 console.log(threeSum([-1,0,1,2,-1,-4,5,6,2,3,5-1,0,-6,-5]))
+
+// var threeSum = function(nums) {
+ 
+//   const result = [];
+  
+//   nums.sort((a, b) => a - b);
+//   for (let i = 0; i < nums.length - 2; i++) {
+//       if (i === 0 || nums[i] !== nums[i - 1]) {
+//           let j = i + 1;
+//           let k = nums.length - 1;
+//           while (j < k) {
+//               if (nums[i] + nums[j] + nums[k] === 0) {
+//                   result.push([nums[i], nums[j], nums[k]]);
+//                   do {
+//                       j++;
+//                   } while (nums[j] === nums[j - 1])
+//                   do {
+//                       k--;
+//                   } while (nums[k] === nums[k + 1])
+//               } else if (nums[i] + nums[j] + nums[k] > 0) {
+//                   k--;
+//               } else {
+//                   j++;
+//               }
+//           }
+//       }
+//   }
+//   return result;
+  
+// };
